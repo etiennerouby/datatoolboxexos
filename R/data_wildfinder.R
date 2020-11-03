478a0180be4a4fcc3d138fbd67dbf53da5696aed
etiennerouby <etiennerouby@orange.fr>
2020-11-03 15:07:04 +0100

    :dolphin: new function

---
+++ R/data_wildfinder.R
@@ -0,0 +1,38 @@
+
+#' Get ecoregion data
+#'
+#' @return data.frame with info on ecoregions
+#' @export
+#'
+#'
+data_ecoregion <- function() {
+
+  ob <- readr::read_csv(here::here("data", "wwf-wildfinder", "wildfinder-ecoregions_list.csv"))
+
+}
+
+#' Get mammal data
+#'
+#' @return data.frame with info on mammals
+#' @export
+#'
+#'
+data_mammals <- function() {
+
+  ob <- readr::read_csv(here::here("data", "wwf-wildfinder", "wildfinder-mammals_list.csv"))
+
+}
+
+#' Get mammal <-> ecoregions data
+#'
+#' @return data.frame with info on which mammal in which ecoregion
+#' @export
+#'
+#'
+data_mammals_ecoregions <- function() {
+
+  ob <- readr::read_csv(here::here("data", "wwf-wildfinder", "wildfinder-ecoregions_species.csv"))
+
+}
+
+
