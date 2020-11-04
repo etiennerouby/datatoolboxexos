30587f44e428e63a39d7e0ba9816fcb424a93e51
etiennerouby <etiennerouby@orange.fr>
2020-11-04 09:27:36 +0100

    :tropical_fish: Organisation

---
+++ make.R
@@ -0,0 +1,20 @@
+###################################################
+#
+# Data and Code for the CESAB Datatoolbox Exercices
+#
+###################################################
+
+# ----- clean workspace
+rm(list = ls())
+
+# ----- install/update packages
+devtools::install_deps()
+
+# ----- install compendium package
+devtools::install(build = FALSE)
+
+# ----- Knit exo dplyr
+rmarkdown::render(here::here("exercices","exo_dplyr.Rmd"))
+
+# ----- Knit exo spatial
+#rmarkdown::render(here::here("exercices","exo_spatial.Rmd"))
